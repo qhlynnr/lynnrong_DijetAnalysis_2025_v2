41ca0df7fac1d1af1af06e8e64662da96c2ef540
qhlynnr <lynnrong@umich.edu>
2025-04-29 19:57:59 +0200

    Updated Plots with labels and correct binning

--- drawMod.c
+++ drawMod.c
@@ -14,25 +14,27 @@ TTree* ReturnTree(const char* inputFileName,string treeName){
     if (!inFile || inFile->IsZombie()) {
         cout << "Error: Could not open the file!" << endl;
     }
-
     TTree* tree = (TTree*)inFile->Get(treeName.c_str());
-
     return tree;
 }
 
-void DrawHistVectorNoTree(vector<float>& dataVec,
-		const char* xLabel = "xLabel",
-		const char* canvasName = "canvasName",
-                const char* histTitle = "default title",
-                const char* outFileName = "default.png",
-                int nbin = 50,
-                int xmin = 0,
-                int xmax = 200,
-		bool isLog = false){
+struct HistVar1D{
+	const char* canvasName = "CanvasName";
+	const char* histTitle = "Hist Title";
+    const char* outFileName = "Plots/Unknown.png";
+	const char* xLabel = "xLabel";
+    int nbin = 50;
+    int xmin = 0;
+    int xmax = TMath::Pi()+0.0001;
+    bool isLog = false;
+};
 
-    TCanvas* canvas = new TCanvas(canvasName, canvasName, 800, 600);
-    TH1F h1("h1", histTitle, nbin, xmin, xmax);
-    if (isLog == true){    
+void DrawHistVectorNoTree(vector<float>& dataVec,
+		HistVar1D HistParam){
+
+    TCanvas* canvas = new TCanvas(HistParam.canvasName, HistParam.canvasName, 800, 600);
+    TH1F h1("h1", HistParam.histTitle, HistParam.nbin, HistParam.xmin, HistParam.xmax);
+    if (HistParam.isLog == true){    
         canvas->SetLogy();
     }
 
@@ -41,9 +43,11 @@ void DrawHistVectorNoTree(vector<float>& dataVec,
     }
    
 
-    h1.SetXTitle(xLabel); // Set the x-axis label
+    h1.SetXTitle(HistParam.xLabel); // Set the x-axis label
     h1.Draw("HIST");
-    canvas->SaveAs(outFileName);
+    canvas->SaveAs(HistParam.outFileName);
+
+    delete canvas;
 }
 void DrawHistFVector(const char* inFileName,
 		     string inTreeName,
@@ -157,6 +161,8 @@ void DrawHistFArray(TTree* tree,
     h1.SetXTitle(branchName); // Set the x-axis label
     h1.Draw("HIST");
     canvas->SaveAs(outFileName);
+
+    delete canvas;
 }
 
 tuple<vector<float>,vector<float>,vector<float>>GetPt1Pt2(TTree* JetAnalyserTree){
@@ -195,7 +201,7 @@ tuple<vector<float>,vector<float>,vector<float>>GetPt1Pt2(TTree* JetAnalyserTree
 		jtpt1vector.push_back(jtpt1);
 		jtpt2vector.push_back(jtpt2);
 		ajvector.push_back(A_J);
-	}
+	    }
     }
     return make_tuple(jtpt1vector,jtpt2vector, ajvector);
 }
@@ -251,6 +257,7 @@ vector<float> GetEmiss(TTree* tree){
 		jptxtot += TMath::Cos(jtphi[j])*jtpt[j];
 		jptytot += TMath::Sin(jtphi[j])*jtpt[j];
 	    }
+        //SUbleading jet with pt>50
 	}
 	EMissjpt = TMath::Sqrt(jptxtot*jptxtot + jptytot*jptytot);
 	EMiss.push_back(EMissjpt);
@@ -302,31 +309,33 @@ void draw2D(TTree* tree){
 	c1->SaveAs("Plots_partialskim_Event5139_ptE2801_Logz.jpg");
 
 }
+struct CutsVector{
+    string name;
+	vector<float> cutVector;
+	float cutmin;
+	float cutmax;
+};
 
+struct CutsBranches{
+    string treename;
+    string name;
+    float cutmin;
+    float cutmax;
+};
 
 //with dphi and eta cuts:
 //In the future make vectors and trees all together
 //automate this so I can feed in arbitrary numbers of vectors and tree branches and it would change the cut and caption accordingly
 void PlotBranchWithCuts(const char* inFileName,
 			const char* branchName,
-			vector<float> cutVector,
-	        	const char* canvasName = "canvasName",
-                	const char* histTitle = "Hist Title",
-                	const char* outFileName = "Plots/PartialSkim_dPhi.png",
-	        	float cutmin = 0,
-	        	float cutmax = 7,
-                	int nbin = 50,
-                	int xmin = 0,
-                	int xmax = TMath::Pi()+0.0001,
-			bool isLog = false){
+			HistVar1D h,
+            CutsVector dPhiCut,
+            CutsBranches etaCut){
 
-    float cut;
-    float etacutmin = 0;
-    float etacutmax = 2;
     int nref, hiBin;
-
-    Float_t jteta[100];
+    float dPhi;
     Float_t branch[100];
+    Float_t jteta[100];
 
     TFile* inFile = new TFile(inFileName,"READ");
     if (!inFile || inFile->IsZombie()) {
@@ -336,70 +345,70 @@ void PlotBranchWithCuts(const char* inFileName,
     TTree* JetAnalyzerTree = (TTree*)inFile->Get("akCs4FlowPFJetAnalyzer/t");
     TTree* HiTree = (TTree*)inFile->Get("hiEvtAnalyzer/HiTree");
 
-    TCanvas* canvas = new TCanvas(canvasName, canvasName, 900, 900);
-    TH1F h1("h1", histTitle, nbin, xmin, xmax);
-    if (isLog == true){
+    TCanvas* canvas = new TCanvas(h.canvasName, h.canvasName, 900, 900);
+    TH1F h1("h1", h.histTitle, h.nbin, h.xmin, h.xmax);
+
+
+    if (h.isLog == true){
         canvas->SetLogy();
     }
 
-    TLatex cuts;
-    TLatex etacuts;
-
-    cuts.SetNDC(); // use normalized device coordinates (0–1)
-    cuts.SetTextSize(0.03);
-
-    etacuts.SetNDC();
-    etacuts.SetTextSize(0.03);
-
+    JetAnalyzerTree->SetBranchAddress(etaCut.name.c_str(),jteta);
     JetAnalyzerTree->SetBranchAddress(branchName,branch);
     JetAnalyzerTree->SetBranchAddress("nref",&nref);
-    JetAnalyzerTree->SetBranchAddress("jteta",jteta);
-    HiTree->SetBranchAddress("hiBin",&hiBin);
 
     for (int i = 0; i < JetAnalyzerTree->GetEntries(); i++){
-	cut = cutVector[i];
-	JetAnalyzerTree->GetEntry(i);
-	HiTree->GetEntry(i);
-	// dPhi Cut here:
-	if (cut > cutmin && cut < cutmax && hiBin < 160){
-	    for (int j = 0; j < nref; j++){
-		// eta cut here:
-		if (TMath::Abs(jteta[j]) < etacutmax){
-		    cout << "Eta: " << jteta[j] << endl;
-		    cout << "Centrality: " << hiBin << endl;
-		    cout << "dPhi:" << cut << endl;
-	            h1.Fill(branch[j]);
-		}
-	    }
-	}
-    }  
+	    JetAnalyzerTree->GetEntry(i);
+	    HiTree->GetEntry(i);
+        dPhi = dPhiCut.cutVector[i];
+        if (dPhi > dPhiCut.cutmin && dPhi < dPhiCut.cutmax){
+            for (int j = 0; j < nref; j++){
+                if (jteta[j] > etaCut.cutmin && jteta[j] < etaCut.cutmax){
+                    cout << "branch: " << branch[j] << endl;
+                    cout << "eta: " << jteta[j] << endl;
+                    cout << "dPhi: " << dPhi << endl;
+                    h1.Fill(branch[j]);
+                }
+            }
+        }
+    }
+
     gStyle->SetOptStat(0);
     gPad->SetTicks();
-    h1.SetXTitle(branchName); // Set the x-axis label
+    h1.SetXTitle(h.xLabel); // Set the x-axis label
     h1.SetYTitle("# of events");
     h1.Draw("HIST E1");
-    
-    if (cutmin > 0 || cutmax < TMath::Pi()){
-        cuts.DrawLatex(0.3, 0.75, Form("dphi: %.2f-%.2f",cutmin,cutmax));
+
+    TLatex cuts1;
+    TLatex cuts2;
+
+    cuts1.SetNDC(); // use normalized device coordinates (0–1)
+    cuts1.SetTextSize(0.03);
+    cuts2.SetNDC();
+    cuts2.SetTextSize(0.03);
+
+    if (dPhiCut.cutmin > 0 || dPhiCut.cutmax < TMath::Pi()){
+        cuts1.DrawLatex(0.3, 0.75, Form("%.2f < %s < %.2f",dPhiCut.cutmin,dPhiCut.name.c_str(),dPhiCut.cutmax));
     }
-    if (etacutmin > 0 || etacutmax < TMath::Pi()){
-    	etacuts.DrawLatex(0.3,0.70, Form("%f < |#eta| < %f",etacutmin,etacutmax));
+    if (etaCut.cutmin > 0 || etaCut.cutmax < TMath::Pi()){
+    	cuts2.DrawLatex(0.3,0.70, Form("%.2f < |%s| < %.2f",etaCut.cutmin,etaCut.name.c_str(),etaCut.cutmax));
     }
-    canvas->SaveAs(outFileName);
+    canvas->SaveAs(h.outFileName);
 }
+
 
 void PlotAJ(const char* inFileName,
 	     vector<float> dphiVector,
 	     const char* canvasName = "canvasName",
-             const char* histTitle = "Jet A_J (cuts dPHi > X)",
-             const char* outFileName = "Plots/PartialSkim_dPhi.png",
+         const char* histTitle = "Jet A_J (cuts dPHi > X)",
+         const char* outFileName = "Plots/PartialSkim_dPhi.png",
 	     int centmin = 0,
 	     int centmax = 200,
 	     float dphimin = 0,
 	     float dphimax = 7,
-             int nbin = 50,
-             int xmin = 0,
-             int xmax = TMath::Pi()+0.0001){
+         int nbin = 50,
+         int xmin = 0,
+         int xmax = TMath::Pi()+0.0001){
     float dphi, A_J;
     int HiBin;
     Float_t jtphi[100];
@@ -463,13 +472,13 @@ void PlotDPhi(const char* inFileName,
   	     string inTreeName,
 	     const char* branchName = "jtphi",
 	     const char* canvasName = "canvasName",
-             const char* histTitle = "dPhi",
-             const char* outFileName = "Plots/PartialSkim_dPhi.png",
+         const char* histTitle = "dPhi",
+         const char* outFileName = "Plots/PartialSkim_dPhi.png",
 	     int centmin = 0,
 	     int centmax = 200,
-             int nbin = 50,
-             int xmin = 0,
-             int xmax = TMath::Pi()+0.0001){
+         int nbin = 50,
+         int xmin = 0,
+         int xmax = TMath::Pi()+0.0001){
 
     float phi1;
     float phi2;
@@ -523,8 +532,6 @@ void PlotDPhi(const char* inFileName,
     h1.Draw("HIST");
 
     latex.DrawLatex(0.5, 0.85, Form("cent: %d-%d%%",TMath::Nint(centmin/2),TMath::Nint(centmax/2)));
-
-
     canvas->SaveAs(outFileName);
 }
 
@@ -642,11 +649,6 @@ void run(const char* inputFileName) {
 
     int nref;
     int HiBin;
-    
-    vector<float>* pfpt = nullptr;
-    vector<float>* pfE = nullptr;
-    vector<float>* pfEta = nullptr;
-    vector<float>* pfPhi = nullptr;
 
     Float_t jtpt[100];
     Float_t jtphi[100];
@@ -660,42 +662,18 @@ void run(const char* inputFileName) {
 
     HiTree->SetBranchAddress("hiBin",&HiBin);
 
-  
-
-/*
-PlotBranchWithCuts(inputFileName, "jtPfCHF",Dphi,"canvas2","jtPfCHF distribution of dphi > 2.7",
-                "Plots/PartialSkim_jtPfCHF_bigdphi.png",
-		TMath::Pi() * (7.0 / 8), TMath::Pi(),
-		50,0,1,true);*/
-
-
-    string branchnamearr[] = {"jtPfCHF","jtPfNEF","jtPfNHF","jtPfCEF","jtPfMUF"};
-
-    float dphimin = 0;
-    float dphimax = 8;
-    string dphicut = "";
-    string dphititle = "";
+    HistVar1D DefaultHistParams{"StructCvsName",
+        "Struct Title",
+        "Plots/TestStruct.png",
+        "xLabel",
+        50,0,1,true};
     
-    for (int i = 0; i < 2; i++){
-	    if (i == 0){
-                dphimin = 0;
-		dphimax = 0.8;
-		dphicut = "smalldphi";
-		dphititle = "with dphi < 0.8";
-	    }
-	    else if (i == 1){
-		dphimin = TMath::Pi() * (7.0 / 8);
-		dphimax = TMath::Pi();
-		dphicut = "bigdphi";
-		dphititle = "with dphi > (7/8)#pi";
-	    }
-	    else if (i == 2){
-		dphimin = 0;
-		dphimax = 10;
-		dphicut = "alldphi";
-		dphititle = "of all jets";
-	    }
-	    for (int j = 0;j < 5 ;j++){
+    CutsVector dPhiCut_low{"dPhi",Dphi, 0, 0.8};
+    CutsVector dPhiCut_high{"dPhi",Dphi, TMath::Pi()*(7.0/8.0), TMath::Pi()};
+    CutsBranches jtEtaCut{"JetAnalyzer", "jteta", 0, 2};        
+    
+   /* for (int i = 0; i < 2; i++){
+	    for (int j = 0;j < branchnamearr.size() ;j++){
 		    string branchname = branchnamearr[j];
 		    string title = "";
 		    string filename = "";
@@ -716,117 +694,8 @@ PlotBranchWithCuts(inputFileName, "jtPfCHF",Dphi,"canvas2","jtPfCHF distribution
 		    cout << filename << endl;
 		    cout << dphimin << " < dphi < " << dphimax << endl;
 		    cout << endl;
-                    PlotBranchWithCuts(inputFileName, 
-				    branchname.c_str(),
-				    Dphi,
-				    title.c_str(),
-				    title.c_str(),
-				    filename.c_str(),
-				    dphimin, dphimax,
-				    50,0,1,true);
-	    }
-    }
-/*
-    PlotDPhi(inputFileName,
-  	    JetAnalyserTreeString,
-	    "jtphi",
-	    "canvasName",
-	    "dPhi",
-	    "Plots/PartialSkim_dphi_cent0to10perc_new.png",
-	    0,
-	    20);
-
-    PlotAJ(inputFileName,Dphi,
-		    "bigdphi1",
-		    "Jet A_{J} (cuts dPHi > (7/8)pi)",
-		    "Plots/PartialSkim_A_J_cent0-10_dphimorethan2p7.png",
-		    0,20,
-		    TMath::Pi() * (7.0 / 8), TMath::Pi(),
-		    50,0,1);
-
-    PlotAJ(inputFileName,Dphi,
-		    "bigdphi2",
-		    "Jet A_{J} (cuts dPHi > (7/8)pi)",
-		    "Plots/PartialSkim_A_J_cent10-30_dphimorethan2p7.png",
-		    20,60,
-		    TMath::Pi() * (7.0 / 8), TMath::Pi(),
-		    50,0,1);
-
-    PlotAJ(inputFileName,Dphi,
-		    "bigdphi3",
-		    "Jet A_{J} (cuts dPHi > (7/8)pi)",
-		    "Plots/PartialSkim_A_J_cent30-80_dphimorethan2p7.png",
-		    60,160,
-		    TMath::Pi() * (7.0 / 8), TMath::Pi(),
-		    50,0,1);
-
-
-    PlotAJ(inputFileName,Dphi,
-		    "smalldphi1",
-		    "Jet A_{J} (cuts dPHi < 0.8)",
-		    "Plots/PartialSkim_A_J_cent0-10_dphilessthan0p8.png",
-		    0,20,
-		    0.0,0.8,
-		    50,0,1);
-
-    PlotAJ(inputFileName,Dphi,
-		    "smalldphi2",
-		    "Jet A_{J} (cuts dPHi < 0.8)",
-		    "Plots/PartialSkim_A_J_cent10-30_dphilessthan0p8.png",
-		    20,60,
-		    0.0,0.8,
-		    50,0,1);
-
-    PlotAJ(inputFileName,Dphi,
-		    "smalldphi3",
-		    "Jet A_{J} (cuts dPHi < 0.8)",
-		    "Plots/PartialSkim_A_J_cent30-80_dphilessthan0p8.png",
-		    60,160,
-		    0.0,0.8,
-		    50,0,1);
-    //draw2D(PFTree);
-
-    string branchnamearr[] = {"pfPt","pfEta","pfPhi"};
-    int xmaxarr[] = {300,6,6};
-    int xminarr[] = {0,-6,-6};
-    bool boolarr[] = {true,false,false};
-
-    const int minPFID = 6;
-    const int maxPFID = 7;//Inclusive i.e. value should be also plotted
-
-    for (int i = minPFID; i <= maxPFID; i++){
-	    for (int j = 0;j < 1;j++){
-		    string branchname = branchnamearr[j];
-		    string title = "";
-		    string filename = "";
-
-		    title += branchname;
-		    title += " of particle flow object pfId = ";
-		    title += to_string(i);
-
-		    filename += "Plots_";
-		    filename += branchname;
-		    filename += "_pfid";
-		    filename += to_string(i);
-		    filename += ".png";
-		    
-		    cout << "pfID: " << i << endl;
-		    cout << "branchName: "<< branchname << endl;
-		    cout << "xmax: " << xmaxarr[j] << endl;
-		    cout << "xmin: " << xminarr[j] << endl;
-		    cout << filename << endl;
-
-		    DrawHistFVector(inputFileName,
-				    treeName,
-				    branchname.c_str(), //must be a float_t array
-				    "nPF",
-				    title.c_str(),
-				    title.c_str(),
-				    filename.c_str(),
-				    300,
-				    xminarr[j],
-				    xmaxarr[j],
-				    i,
-				    boolarr[j]);}
-    }*/
+            PlotBranchWithCuts(inputFileName, "jtPfCHF",
+                DefaultHistParams, dPhiCut_low, jtEtaCut);        
+        }
+    }  */  
 }
